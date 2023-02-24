f3eafb2e3bf7e3a129351def20be07683840732f
Wellington Cavalcante <wellarts@gmail.com>
2023-02-24 17:19:35 -0300

    testes

--- resources/views/filament/contasReceber/footer.blade.php
+++ resources/views/filament/contasReceber/footer.blade.php
@@ -1,7 +1,7 @@
-<td colspan="5" class="px-4 py-3 filament-tables-text-column">
+<td colspan="5" class="px-4 py-3 filament-tables-text-column" alignment="right">
     Total:
 </td>
-<td class="filament-tables-cell" alignment="center">
+<td class="filament-tables-cell" alignment="right">
     <div class="px-4 py-3 filament-tables-text-column">
        Valor das Parcelas: R$ {{($this->getTableRecords()->sum('valor_parcela')) }} <br>
        Valor Recebido: R$ {{($this->getTableRecords()->sum('valor_recebido')) }}
