cb118635784fa00ade3b97a25bbad715ae35ec6a
Wellington Cavalcante <wellarts@gmail.com>
2023-02-24 22:10:40 -0300

    saldo

--- resources/views/filament/contasReceber/footer.blade.php
+++ resources/views/filament/contasReceber/footer.blade.php
@@ -1,11 +1,22 @@
-<td colspan="5" class="px-4 py-3 filament-tables-text-column" alignment="right">
-    Total:
-</td>
-<td class="filament-tables-cell" alignment="right">
-    <div class="px-4 py-3 filament-tables-text-column">
-       Valor das Parcelas: R$ {{($this->getTableRecords()->sum('valor_parcela')) }} <br>
-       Valor Recebido: R$ {{($this->getTableRecords()->sum('valor_recebido')) }}
-    </div>
 
-</td>
+ <table @class("border border-gray-300 w-full text-right table-auto font-bold bg-stone-400")>
+
+    <tr>
+        <td class="filament-tables-cell text-2xl">
+         Resumo:
+       </td>
+
+            <td class="filament-tables-cell text-bg" alignment="right">
+
+                    Valor das Parcelas: R$ {{($this->getTableRecords()->sum('valor_parcela')) }} <br>
+                    Valor Recebido: R$ {{($this->getTableRecords()->sum('valor_recebido')) }} <br>
+                    ____________________ <br>
+                    Saldo: R$  {{($this->getTableRecords()->sum('valor_recebido')) - ($this->getTableRecords()->sum('valor_parcela')) }}
+              
+            </td>
+
+    </tr>
+
+</table>
+
 
